393ac273bd16fc251060211a9fa8646834d85581
Ashfaque Khowaja <Khowaja.ashfaqali@hotmail.com>
2024-10-17 16:27:28 +0800

    Initial commit for jekyll site

---
+++ assets/js/functions.js
@@ -0,0 +1,9 @@
+// assets/js/scripts.js
+window.addEventListener('scroll', function() {
+    const navbar = document.querySelector('.navbar');
+    if (window.scrollY > 0) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
